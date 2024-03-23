940fb30fea8a55e73b0f0afc2dd3320b8cfd601b
andiirc <andiirc@gmail.com>
2024-03-23 16:09:03 -0500

    create data products

---
+++ src/shared/data/products.ts
@@ -0,0 +1,10 @@
+import { Product } from '../../entities/Product';
+import { v4 as uuidv4 } from 'uuid';
+
+const products: Product[] = [
+  { id: uuidv4(), name: 'Product 1', price: 100 },
+  { id: uuidv4(), name: 'Product 2', price: 200 },
+  { id: uuidv4(), name: 'Product 3', price: 300 },
+];
+
+export { products }
