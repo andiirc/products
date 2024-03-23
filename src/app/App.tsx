e176177f68f1e2b7830cff33123de33dd4ce5dfa
andiirc <andiirc@gmail.com>
2024-03-23 16:03:30 -0500

    create homepage and improvement structure folder

---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react'
+import { HomePage } from '../pages/Home/HomePage'
+//import reactLogo from '/react.svg'
+//import viteLogo from '/vite.svg'
+//import '../App.css'
+
+export const App:FC = () => {
+  return (
+    <>
+      <HomePage />
+    </>
+  )
+}
---
+++ src/pages/Home/HomePage.tsx
@@ -0,0 +1,48 @@
+
+import { FC, useEffect, useState } from 'react'
+import { ListProducts } from '../../features/products/components/ListProducts'
+import { ProductRepository } from '../../features/products/adapters/out/data/ProductRepository';
+import { GetProductUseCase } from '../../features/products/usecases/GetProductUseCase';
+import { SaveProductUseCase } from '../../features/products/usecases/SaveProductUseCase';
+import { Product } from '../../entities/Product';
+
+const HomePage:FC = (): JSX.Element => {
+  const [product, setProduct] = useState<Product>({name: '', price: 0});
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    handleLoadProducts();
+  },[products]);
+
+  const handleLoadProducts = () => {
+    const productsReposiroty = new ProductRepository();
+    const getProductsUseCase = new GetProductUseCase(productsReposiroty);
+    const filters = {};
+    const listProducts = getProductsUseCase.apply(filters);
+    setProducts(listProducts);
+  }
+
+  const handleChange = (key: string, value: any) => {
+    setProduct({ ...product, [key]: value.target.value });
+  };
+
+  const handleAddProduct = (product: Product) => {  
+    const productsReposiroty = new ProductRepository();
+    const saveProductUseCase = new SaveProductUseCase(productsReposiroty);
+    saveProductUseCase.apply(product);
+    setProducts([...products, product]);
+    setProduct({name: '', price: 0});
+  }
+
+  return (
+    <div>
+      <p> Crear Productos</p>
+        <input type="text" name="name" value={product.name} onChange={(text) => handleChange('name', text)}/>
+        <input type="number" name="price" value={product.price} onChange={(text) => handleChange('price', text)}/>
+        <button onClick={() => handleAddProduct(product)}>Crear</button>
+        <ListProducts products={products} />
+    </div>
+  );
+}
+
+export { HomePage }
