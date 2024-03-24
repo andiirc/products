6f46ea57140155bfb9b4c6d20910477f5a24c08e
andiirc <andiirc@gmail.com>
2024-03-23 23:08:28 -0500

    config prefix module src

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig, splitVendorChunkPlugin } from 'vite'
 import react from '@vitejs/plugin-react'
-
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": '/src',
+    },
+  },
   build: {
     outDir: 'dist',
     rollupOptions: {
@@ -13,4 +16,5 @@ export default defineConfig({
       },
     },
   },
+  plugins: [react(), splitVendorChunkPlugin()],
 })
