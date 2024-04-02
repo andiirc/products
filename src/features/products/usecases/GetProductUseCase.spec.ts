575e0287550ecbdf7fad081eb24a9ad8e7f47ea7
andiirc <andiirc@gmail.com>
2024-04-02 15:20:07 -0500

    test unit get products usecase

---
+++ src/features/products/usecases/GetProductUseCase.spec.ts
@@ -0,0 +1,35 @@
+import {describe, it, expect, beforeEach } from 'vitest';
+import { GetProductUseCase } from "./GetProductUseCase";
+import { IProductRepository } from "src/features/products/interfaces/IProductRepository";
+import { MockProductRepository } from 'src/features/products/interfaces/MockProductRepository';
+
+describe('GetProductUseCase', () => {
+  let getProductUseCase: GetProductUseCase;
+  let mockUserRepository: IProductRepository;
+
+  beforeEach(() => {
+    mockUserRepository = new MockProductRepository();
+    getProductUseCase = new GetProductUseCase(mockUserRepository);
+  })
+
+  it('should return all products', () => {
+    const products = getProductUseCase.apply({});
+    expect(products).toHaveLength(3);
+  });
+
+  it('should return products filtered by name', () => {
+    const products = getProductUseCase.apply({ name: 'Product 1' });
+    expect(products).toHaveLength(1);
+
+    expect(products[0].name).toBe('Product 1');
+    expect(products[0].price).toBe(100);
+  });
+
+  it('should return products filtered by price', () => {
+    const products = getProductUseCase.apply({ price: 300 });
+  
+    expect(products).toHaveLength(1);
+    expect(products[0].name).toBe('Product 3');
+    expect(products[0].price).toBe(300);
+  });
+});
