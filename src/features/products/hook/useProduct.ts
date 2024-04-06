0b857a86f5e54c5e7db22f5436cb9819eb61eee8
andiirc <andiirc@gmail.com>
2024-04-05 19:27:45 -0500

    custom hook products

---
+++ src/features/products/hook/useProduct.ts
@@ -0,0 +1,41 @@
+import { useState, useEffect } from 'react';
+import { Product } from 'src/entities/Product';
+import { ProductUseCaseManager } from 'src/features/products/usecases/ProductUseCasesManager';
+
+const useProduct = () => {
+  const [product, setProduct] = useState<Product>({name: '', price: 0});
+  const [products, setProducts] = useState<Product[]>([]);
+  const { getProducts, createProduct } = ProductUseCaseManager
+
+  useEffect(() => {
+    handleLoadProducts();
+  },[products]);
+
+  const handleLoadProducts = (): void => {
+    const listProducts = getProducts();
+    setProducts(listProducts);
+  }
+
+  const handleChange = (key: string, value: any): void => {
+    setProduct({ ...product, [key]: value.target.value });
+  };
+
+  const handleAddProduct = (event: any): void => {
+    event.preventDefault();
+    createProduct(product);
+    setProducts([...products, product]);
+    setProduct({name: '', price: 0});
+  }
+
+  const isFormValid: boolean = product.name.trim() !== '' && product.price > 0;
+
+  return {
+    product,
+    products,
+    handleChange,
+    handleAddProduct,
+    isFormValid
+  }
+}
+
+export { useProduct };
